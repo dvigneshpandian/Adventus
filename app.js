2538c71db7f77d4724f80ce6e836992faed95384
Vignesh Pandian <vignesh.duraipandi@gmail.com>
2013-10-18 14:36:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-//Yeah
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
